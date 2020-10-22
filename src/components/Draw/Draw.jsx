68fc16e8a4cbc0bf3b2a10d5b1b1b68e3e0af124
Jominhui <rihito2021@naver.com>
2020-10-22 12:07:20 +0900

    [UPDATE] eraser

--- src/components/Draw/Draw.jsx
+++ src/components/Draw/Draw.jsx
@@ -23,9 +23,21 @@ const Draw = ({}) => {
     setEndPath([e.offsetX, e.offsetY]);
   }, [])
 
+  const eraserDown = useCallback((e, ctx) => {
+    ctx.beginPath();
+    ctx.lineWidth = 10;
+    ctx.moveTo(e.offsetX, e.offsetY);
+  }, [])
+
   const strokeMove = useCallback((e, ctx) => {
     ctx.lineTo(e.offsetX, e.offsetY);
     ctx.strokeStyle = "black";
+    ctx.stroke();
+  }, [])
+
+  const eraserMove = useCallback((e, ctx) => {
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.strokeStyle = "white";
     ctx.stroke();
   }, [])
 
@@ -56,6 +68,9 @@ const Draw = ({}) => {
       case "stroke":
         strokeDown(e, ctx);
         break;
+      case "eraser":
+        eraserDown(e, ctx);
+        break;    
       case "square":
         squareDown(e, ctx);
         break;
@@ -71,6 +86,9 @@ const Draw = ({}) => {
       case "stroke":
         strokeMove(e, ctx);
         break;
+      case "eraser":
+        eraserMove(e, ctx);
+        break;
       case "square":
         squareMove(e, ctx);
         break;
@@ -83,6 +101,8 @@ const Draw = ({}) => {
 
     switch(tool) {
       case "stroke":
+        break;
+      case "eraser":
         break;
       case "square":
         drawSquare(e, ctx);
@@ -115,9 +135,9 @@ const Draw = ({}) => {
         <div className="Tools">
           <div className="Tools-Title">Tools</div>
           <div className="Tools-menu" onClick={() => setTool("stroke")}>선</div>
-          <div className="Tools-menu">지우개</div>
+          <div className="Tools-menu" onClick={() => setTool("eraser")}>지우개</div>
           <div className="Tools-menu" onClick={() => setTool("square")}>사각형</div>
-          <div className="Tools-menu">삼각형</div>
+          <div className="Tools-menu" onClick={() => setTool("eraser")}>삼각형</div>
           <div className="Tools-menu">원</div>
           <div className="Tools-menu">이미지</div>
           <div className="Tools-menu">저장</div>
