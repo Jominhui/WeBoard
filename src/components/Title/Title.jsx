358c45047f7a78967338962e350a018e2e6339b3
Jominhui <rihito2021@naver.com>
2020-10-28 14:29:23 +0900

    [UPDATE] make fadein animation

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -9,7 +9,7 @@ const Title = ({}) => {
       <div className="Title">
         <p className="Title-coment">환영합니다!</p>
         <p className="Title-coment">당신의 그림을 WEBOARD 그려주세요.</p>
-        <input className="Title-box" />
+        <input className="Title-box" autoFocus />
         
         <Link to="/Draw">
           <div className="Title-btn">
