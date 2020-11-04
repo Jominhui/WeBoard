dc18d21170ec3056dc76c7dd2c9992a9ce61af8c
Jominhui <rihito2021@naver.com>
2020-11-04 15:11:07 +0900

    [UPDATE] NextPage Title

--- src/containers/Title/TitleContainer.js
+++ src/containers/Title/TitleContainer.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { inject, observer } from "mobx-react";
 import Title from "components/Title/index"
+import { useHistory, withRouter } from "react-router-dom";
 
 const MainContainer = () => {
+  const history = useHistory();
+
+  const [title, setTitle] = useState("");
+
+  const nextPage = useCallback(() => {
+    if (title == "") {
+      return;
+    }
+
+    localStorage.setItem("name", title);
+    history.push("/")
+  }, [title]);
+
   return (
     <>
-      <Title />
+      <Title title={title} setTitle={setTitle} nextPage={nextPage} />
     </>
   );
 };
 
-export default inject("store")(observer(MainContainer));
+export default withRouter(observer(MainContainer));
